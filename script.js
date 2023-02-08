99e958929049a5d57a524707f08a212de228bdcd
Leon Graff <442graff@gmail.com>
2023-02-08 20:03:30 -0300

    calc1

---
+++ script.js
@@ -0,0 +1,42 @@
+/*class calcular { //giving the name
+    constructor() { //what does the constructor really do here?
+
+    }
+}*/
+
+var el = document.getElementById('fora');
+el.addEventListener('click', function (e) {
+    alert(e.target.id);
+});
+
+
+
+
+
+
+/*function getvalue() {
+
+    var value = document.getElementById("number").innerHTML;
+    console.log(value);
+    var value2 = document.getElementById("number2").innerHTML;
+    console.log(value2)
+
+
+}
+
+var value2 = document.getElementById("number2").innerHTML;
+console.log(value2);
+var value3 = document.getElementById("number3").innerHTML;
+console.log(value3);
+var value4 = document.getElementById("number4").innerHTML;
+console.log(value4);
+var value5 = document.getElementById("number5").innerHTML;
+console.log(value5);
+var value6 = document.getElementById("number6").innerHTML;
+console.log(value6);
+var value7 = document.getElementById("number7").innerHTML;
+console.log(value7);
+var value8 = document.getElementById("number8").innerHTML;
+console.log(value8);
+var value9 = document.getElementById("number9").innerHTML;
+console.log(value9);*/
