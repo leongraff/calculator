29c6ead9c5ed2b02acb08d9ddf79da8585537e03
Leon Graff <442graff@gmail.com>
2023-03-09 20:03:20 -0300

    first commit

--- script.js
+++ script.js
@@ -1,17 +1,145 @@
+
+//array pode conter varios objetos
+/*var caipira = [
+    {x:limao y:gelo},
+    {z:vodka k:copo}
+]
+
+console.log(caipira) */
+
+//objeto pode conter varias arrays
+
+/*
+var x = 1;
+var y = 2;
+
+var a1 = x;
+var count = y;
+
+count *= 3;
+
+console.log(count)
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*class calcular { //giving the name
     constructor() { //what does the constructor really do here?
 
     }
 }*/
 
-var el = document.getElementById('fora');
+/*var el = document.getElementById('fora');
 el.addEventListener('click', function (e) {
-    alert(e.target.id);
+    var numberId = e.target.id;
+    var valor = document.getElementById(numberId).innerHTML;
+    var x = valor;
+
+    var z = [];
+    function addNumber(){
+        z.push(x);
+    }
+    addNumber();
+    console.log(z);
+    console.log(x);
+
+
+
 });
+function caipiraBoa(){
+
+var caipira = {
+    gelo: "importante",
+    limao: "essencial",
+    boa: true 
+}   
+if (caipira.gelo = "importante"){
+    console.log("quero beber!!!")
+}
+}
+caipiraBoa()
+
+
+
+var caipirinha = ["limao","açucar","gelo"];
+console.log(caipirinha[0]); //primeiro elemento sempre é zero
+console.log(caipirinha.lenght); //tamanho da caipirinha
+console.log(caipirinha.lenght-1); //pega ultimo elemento?
+caipirinha[3] = "cachaça"; //adiciona elemento na array
+console.log(caipirinha) //print na array
+caipirinha[3]="nada"; //muda elemento existente por meio da atribuição
+console.log(caipirinha)
+
+var empty = [];
+console.log(empty.lenght)
 
 
 
 
+
+
+
+
+
+
+
+
+
+var book = {
+    topic: "Javascript", //passando valores assim, se usa virgula
+    fat: true
+
+}
+console.log(book.topic)
+console.log(book["fat"])
+book.author = "Flanagan";
+console.log(book.author)
+book.contents = ["jesus","é","amor"]
+console.log(book.contents)
+book.vazio = {}; //objeto vazio sem valor /*
+
+
+// sera q  : é a mesma coisa que = ??
+
+
+/*function factorial (n){
+    var p = 1;
+    for (var i = 2; i <= n; i++)
+    p - i;
+    return i;
+}
+
+console.log(factorial(4));
+console.log(factorial(7))
+
+*/
 
 
 /*function getvalue() {
---
+++ script2.js
@@ -0,0 +1,14 @@
+const buttons = document.querySelectorAll("button");
+
+buttons.addEventListener("click", function () {
+    const valorBotao = buttons.innerHTML;
+    console.log(valorBotao);
+})
+
+//console.log(button);
+
+function somar() {
+    if (buttons += 0) {
+        console.log(buttons);
+    }
+}
